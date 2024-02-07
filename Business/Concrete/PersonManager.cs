1749d8dd6d51254d0e7f33a842484d03abff68f9
omerblgn <omerbilgen01@gmail.com>
2024-02-07 14:16:02 +0300

    Add Mernis Service Reference

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+        public List<Person> GetList()
+        {
+            return new List<Person>();
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            return true;
+        }
+    }
+}
